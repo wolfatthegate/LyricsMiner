823cde0fe3c3f813c112f15399319907e41be42d
Wolfie Codes <58965905+wolfatthegate@users.noreply.github.com>
2021-03-14 20:51:07 -0400

    update updateMongodb

--- src/dbQueries/UpdateMongoDB.py
+++ src/dbQueries/UpdateMongoDB.py
@@ -1,21 +1,29 @@
 import pymongo
 import sys
+import concurrent.futures
+import warnings
+warnings.filterwarnings("ignore")
  
+def updateTweet(mydoc):
+    updatequery = {'_id': mydoc['_id']}
+    newvalue = { '$set': {'excluded': 0}}
+    lyricTbl.update_one(updatequery, newvalue)
+    
 myclient = pymongo.MongoClient("mongodb://localhost:27017/")
 
 mydb = myclient['TwitterData']
 lyricTbl = mydb['2016-11-Nov-week-1']
 
-myquery = {}
+myquery = {'excluded': {'$ne': 0}}
 noofdoc = lyricTbl.find(myquery).count()
 
 x = 0
-inc = 5
+inc = 100
+
 while x < noofdoc:
-    for mydoc in lyricTbl.find(myquery).skip(x).limit(inc): #find() method returns a list of dictionary
-        updatequery = {'_id': mydoc['_id']}
-        newvalue = { '$set': {'excluded': 0}}
-        lyricTbl.update_one(updatequery, newvalue)
+    docs = lyricTbl.find(myquery).skip(x).limit(inc) #find() method returns a list of dictionary
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        executor.map(updateTweet, docs)
     x = x + inc 
 
 
