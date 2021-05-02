fc5f6b4678f9b03f6545767ccbc6b77fa0cb1380
waylonluo <waylonluo@192.168.0.19>
2021-05-02 12:30:43 -0400

    New class PrintRandTweets

---
+++ src/dbQueries/PrintRandTweets.py
@@ -0,0 +1,41 @@
+import sys
+import random
+import pymongo
+
+def main():
+    cleaner = TextCleaner.TextCleaner()
+    
+    tablename = sys.argv[1] # '2016-11-Nov-week-1'
+    filename = "Rand1000-"+ tablename + "-" + sys.argv[2] +".txt"
+    
+    myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+    myclient.list_database_names()
+    
+    twitterDataTbl = myclient['TwitterData']
+    twitterDataTbl.list_collection_names()
+    
+    tweetTbl = twitterDataTbl[tablename]
+    myquery = {}
+    
+    x = 0
+    docs = tweetTbl.find(myquery)
+    tweetCount = tweetTbl.find(myquery).count()
+    
+    sample_size = sys.argv[2]
+    if tweetCount < sample_size: 
+        sample_size = tweetCount
+    
+    list_docs = list(docs)
+    
+    random.shuffle(list_docs)
+    randocs = random.sample(list_docs, sample_size)
+    counter = 0
+    
+    with open("exportedData/" + filename, "a") as file:
+        for randoc in randocs: 
+        
+            tweet = randoc['tweet'].replace('n', '')
+            file.write(tweet+'\n')
+            
+if __name__ == "__main__":
+    main()
