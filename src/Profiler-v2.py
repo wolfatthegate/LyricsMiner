26a9bd9324082efacb6a13a37552882ac4615c18
waylonluo <waylonluo@192.168.0.10>
2021-07-06 17:21:50 -0400

    profiler version2

---
+++ src/Profiler-v2.py
@@ -0,0 +1,44 @@
+import pymongo
+import sys
+
+def main(): 
+
+    ### DB connection
+
+    table_name = '2016-10-week-1'
+    table_name_1 = '2016-10-week-1-1'
+    
+    myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+    twitterDB = myclient['TwitterData']
+    tweetTbl = twitterDB[table_name]
+    tweetTbl_1 = twitterDB[table_name_1]
+    
+    ### Query
+    
+    dataList = tweetTbl.find({'score': {'$gt': 0.49}})
+    
+    ### initialization
+    
+    user_dict = {}
+
+    for data in dataList: 
+        tweet = tweetTbl_1.find({'tweetID': data['tweetID']})
+        
+        user_id =  tweet['userid']
+        if user_id in user_dict.keys():
+            continue
+        
+        tweets = []
+        userdataList = tweetTbl_1.find({'userid': user_id})
+        
+        for userdata in userdataList: 
+            tweets.append(userdata['tweet'].replace('\n', ' '))
+        user_dict[data['userid']] = tweets
+    
+    with open('logs/'+table_name+'profiles.log', 'a') as profile: 
+        for key, value in user_dict.items(): 
+            profile.write( '{}\t{}\n'.format(key, '\n\t'.join(value)))    
+    
+if __name__ == "__main__":
+    main()
+
