a6abcd8829d41a8215d4db8b9848afce563c0816
waylonluo <waylonluo@192.168.0.10>
2021-07-06 16:54:13 -0400

    profiler update

--- src/Profiler.py
+++ src/Profiler.py
@@ -5,10 +5,12 @@ def main():
 
     ### DB connection
 
-    table_name = sys.argv[1] ###'2017-10-week-1'
+    table_name = '2016-10-week-1'
+    table_name_1 = '2016-10-week-1-1'
     myclient = pymongo.MongoClient("mongodb://localhost:27017/")
     twitterDB = myclient['TwitterData']
     tweetTbl = twitterDB[table_name]
+    tweetTbl_1 = twitterDB[table_name_1]
     
     ### Query
     
@@ -19,7 +21,7 @@ def main():
     user_dict = {}
 
     for data in dataList: 
-        userdataList = tweetTbl.find({'userid': data['userid']})
+        userdataList = tweetTbl_1.find({'userid': data['userid']})
         
         user_id =  data['userid']
         if user_id in user_dict.keys():
