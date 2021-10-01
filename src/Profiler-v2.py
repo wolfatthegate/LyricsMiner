645f1b8a6c8bb80b2be1409ec3cce8a67cbc2c8a
Wolf Walker <58965905+wolfatthegate@users.noreply.github.com>
2021-10-01 02:24:58 -0400

    Update Profiler-v2.py

--- src/Profiler-v2.py
+++ src/Profiler-v2.py
@@ -1,17 +1,19 @@
+### Profiler list the Lyrics tweeters and the followers to measure the influence. 
+### Author - Waylon Luo
+
+
 import pymongo
 import sys
+import pandas as pd
 
 def main(): 
 
     ### DB connection
 
-    table_name = '2016-10-week-3'
-    table_name_1 = '2016-10-week-3-1'
-    
+    table_name = sys.argv[1] #'2017-10-week-1'
     myclient = pymongo.MongoClient("mongodb://localhost:27017/")
     twitterDB = myclient['TwitterData']
     tweetTbl = twitterDB[table_name]
-    tweetTbl_1 = twitterDB[table_name_1]
     
     ### Query
     
@@ -19,37 +21,13 @@ def main():
     
     ### initialization
     
-    user_dict = {}
+    usrlist = ['userid', 'follower', 'tweet']
 
-    for data in dataList: 
-        tweet = tweetTbl_1.find({'tweetID': data['tweetID']})
-        
-        if tweet.count() > 0:
-            if 'userid' in tweet[0]:
-                user_id =  tweet[0]['userid']
-            else:
-                continue
-            if 'follower' in tweet[0]:
-                follower = tweet[0]['follower']
-            else: 
-                follower = 0
-        else:
-            continue
-        if user_id in user_dict.keys():
-            continue
-        
-        tweets = []
-        userdataList = tweetTbl_1.find({'userid': user_id})
-        
-        for userdata in userdataList: 
-            tweets.append(str(follower) + '\t' + userdata['tweet'].replace('\n', ' '))
-        user_dict[user_id] = tweets
+    for data in dataList:      
+        usrlist.append(data['userid'], data['follower'], data['tweet'])
     
-    with open('logs/'+table_name+'profiles.log', 'a') as profile: 
-        profile.write('{}\t{}\t{}\n'.format('user id', 'follower', 'tweet'))
-        for key, value in user_dict.items(): 
-            profile.write( '{}\t{}\n'.format(key, '\n\t'.join(value)))    
+    df = pd.DataFrame(usrlist)
+    df.to_csv(file_name+'-profiles.csv', index = False)
     
 if __name__ == "__main__":
     main()
-
