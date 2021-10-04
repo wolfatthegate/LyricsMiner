d6e3b5fd3386fb4a388c54a8f86d587cb137cb02
Wolf Walker <58965905+wolfatthegate@users.noreply.github.com>
2021-10-04 01:33:05 -0400

    Update mainv2.py

--- src/mainv2.py
+++ src/mainv2.py
@@ -17,30 +17,24 @@ def main():
     print('total number of docs in query - {}'.format(noofdoc))
     parallel = 0 ### 0 for serial program ### 1 for parallel program  
     scanned = 0
-    y = 200
     
-    while noofdoc > scanned:
-        
-        docs = DBv2.tweetTbl.find(myquery).skip(y).limit(y)
-    
-        if parallel == 0: ###   RUN IN SERIAL 
-            for doc in docs: 
-                LyricsMinerv2.searchTweet(doc)
-            
-        else:             ###   RUN IN PARALLEL 
-            with concurrent.futures.ProcessPoolExecutor() as executor:
-                executor.map(LyricsMinerv2.searchTweet, docs)
-       
-        scanned += y
-        
-        if scanned%(2000) == 0 :  ###   Write a log after every x document
-            with open('logs/'+ DBv2.tablename +'Log.log', 'a') as logfile:
-                now = datetime.now()
-                dt_string = now.strftime("%m/%d/%Y %H:%M:%S")
-                logfile.write('{}: {} scanned {} documents \n'.format(DBv2.tablename, dt_string, scanned)) 
-                print('{}: {} scanned {} documents \n'.format(DBv2.tablename, dt_string, scanned))
-        
+    docs = DBv2.tweetTbl.find(myquery)
 
+    if parallel == 0: ###   RUN IN SERIAL 
+        for doc in docs: 
+            LyricsMinerv2.searchTweet(doc)
+            scanned += 1
+            if scanned%(2000) == 0 :  ###   Write a log after every x document
+                with open('logs/'+ DBv2.tablename +'Log.log', 'a') as logfile:
+                    now = datetime.now()
+                    dt_string = now.strftime("%m/%d/%Y %H:%M:%S")
+                    logfile.write('{}: {} scanned {} documents \n'.format(DBv2.tablename, dt_string, scanned)) 
+                    print('{}: {} scanned {} documents \n'.format(DBv2.tablename, dt_string, scanned))
+        
+    else:             ###   RUN IN PARALLEL 
+        with concurrent.futures.ProcessPoolExecutor() as executor:
+            executor.map(LyricsMinerv2.searchTweet, docs)
+   
 if __name__ == "__main__":
     print('start main - scanning {}'.format(DBv2.tablename))
     start = time.time()
