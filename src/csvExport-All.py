85dcff2beb96ab800c7a496cd9580c022abeb9ba
Wolf Walker <58965905+wolfatthegate@users.noreply.github.com>
2021-11-16 22:40:58 -0500

    Update csvExport-All.py

--- src/csvExport-All.py
+++ src/csvExport-All.py
@@ -27,13 +27,21 @@ tableList = ['2016-01-week-2', '2016-01-week-3', '2016-01-week-4',
 '2017-10-week-1', '2017-10-week-2', '2017-10-week-3', '2017-10-week-4',
 '2017-11-week-1', '2017-11-week-2', '2017-11-week-3', '2017-11-week-4',
 '2017-12-week-1', '2017-12-week-2', '2017-12-week-3', '2017-12-week-4'] 
-filename = '-All-Lyrics'
+filename = '2016-2017-All-Lyrics'
 
 myclient = pymongo.MongoClient("mongodb://localhost:27017/")
 twitterDataTbl = myclient[dbname]
 
-for table in tableList: 
+
+for idx, table in enumerate(tableList): 
     tweetTbl = twitterDataTbl[table]
     cursor = tweetTbl.find({'score':{'$gt':0.49}})    
     df = pd.DataFrame(list(cursor))
-    df.to_csv(table + filename + '.csv', index=False)
+    
+    if idx == 0:
+        total_df = df
+    else:
+        frames = [total_df, df]
+        total_df = pd.concat(frames)
+        
+total_df.to_csv(filename + '.csv', index=False)
