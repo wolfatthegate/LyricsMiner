dd992dce90a028b0e5c488f8794af42435884f0f
WLUO <58965905+wolfatthegate@users.noreply.github.com>
2022-06-08 20:25:04 -0400

    Update mainv2.py

--- src/mainv2.py
+++ src/mainv2.py
@@ -11,35 +11,36 @@ from datetime import datetime
 def main():
     
     ### Initialization
+    ### get the number of document which are already scanned.    
     myquery = {}
+    count = 0
+     
+    lycount = DBv2.lyricsMatchTbl.find().count()
+    cursor = DBv2.tweetTbl.find(myquery).skip(lycount) #find() method returns a list of dictionary
     
-    noofdoc = DBv2.tweetTbl.find(myquery).count() #find() method returns a list of dictionary
-    print('total number of docs in query - {}'.format(noofdoc))
-    parallel = 0 ### 0 for serial program ### 1 for parallel program  
-    scanned = 0
-    
-    docs = DBv2.tweetTbl.find(myquery)
-
-    if parallel == 0: ###   RUN IN SERIAL 
-        for doc in docs: 
+    while cursor.alive: 
+        try: 
+            doc = cursor.next()
             LyricsMinerv2.searchTweet(doc)
-            scanned += 1
-            if scanned%(2000) == 0 :  ###   Write a log after every x document
+            count+=1
+            
+            if count%(500) == 0 :  ###   Write a log after every x document
                 with open('logs/'+ DBv2.tablename +'Log.log', 'a') as logfile:
                     now = datetime.now()
                     dt_string = now.strftime("%m/%d/%Y %H:%M:%S")
-                    logfile.write('{}: {} scanned {} documents \n'.format(DBv2.tablename, dt_string, scanned)) 
-                    print('{}: {} scanned {} documents \n'.format(DBv2.tablename, dt_string, scanned))
-        
-    else:             ###   RUN IN PARALLEL 
-        with concurrent.futures.ProcessPoolExecutor() as executor:
-            executor.map(LyricsMinerv2.searchTweet, docs)
-   
+                    logfile.write('{}: {} scanned {} documents from table {}\n'.format(DBv2.tablename, dt_string, count, DBv2.tablename)) 
+                    print('{}: {} scanned {} documents from table {}\n'.format(DBv2.tablename, dt_string, count, DBv2.tablename))    
+            
+        except StopIteration:
+            print('error in mainv2')
+            time.sleep(1)
+            
+    print('total number of docs inserted - {}'.format(count))
+    print('done..')
+
 if __name__ == "__main__":
     print('start main - scanning {}'.format(DBv2.tablename))
     start = time.time()
     main()
     stop = time.time()
     print("total time: {}".format(stop-start))
-                                
-
